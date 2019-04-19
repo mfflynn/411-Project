8af0af99c22c10e8934446dbf635cfa695f53dff
James Sweeney <jdswee@bu.edu>
2019-04-19 14:18:38 -0400

    added mongod

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-//test
 const cf = require('./config').config;
 const express = require('express');
 const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
@@ -120,3 +119,12 @@ app.use('/', router);
 app.listen(process.env.port || 3000);
 
 console.log('Running at Port 3000');
+
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017";
+
+MongoClient.connect(url, function(err, db) {
+    if (err) throw err;
+    console.log("Database created!");
+    db.close();
+});
